9e54cadfd921faa6de01884f724cb369e4c31105
hai061898 <truonghaiauto98@gmail.com>
2022-03-31 18:46:04 +0700

    model

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,44 @@
+class User {
+  String? avatar;
+  String? username;
+  String? name;
+
+  User({
+    this.avatar,
+    this.username,
+    this.name,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        avatar: json['avatar'] as String?,
+        username: json['username'] as String?,
+        name: json['first_name'] + ' ' + json['last_name'] as String?,
+      );
+}
+
+class UserModel {
+  String? id;
+  String? username;
+  String? name;
+  String? email;
+  String? city;
+  String? avatar;
+
+  UserModel({
+    this.id,
+    this.username,
+    this.name,
+    this.email,
+    this.city,
+    this.avatar,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        id: json['id'] as String?,
+        username: json['username'] as String?,
+        name: json['first_name'] + ' ' + json['last_name'] as String?,
+        email: json['email'] as String?,
+        city: json['city'] as String?,
+        avatar: json['avatar'] as String?,
+      );
+}
---
+++ lib/data/services/url_api.dart
@@ -0,0 +1,3 @@
+const String baseUrl = '127.0.0.1';
+
+const String basePath = '/api/v1';
